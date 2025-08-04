372f01f68fe8ef1c4583ac8ec57ad22a51c8d7e9
Richard Cannon <richard.cannon@ed.ac.uk>
2025-08-04 13:47:09 +0100

    .

--- NEW/observability_finder.py
+++ NEW/observability_finder.py
@@ -6,18 +6,17 @@ def observability_finder():
 
 #===Functions for parsing args below this point===
 def parse_args():
-    """Parse command-line arguments."""
-    parser = argparse.ArgumentParser(description="Check the observability of a list of solar system objects",
-                                     epilog='Cannot accept limits in both airmass and elevation')
-    parser.add_argument("-v", "--verbose", action="store_true",
-                        help="Enable verbose output (sets log level to DEBUG)")
+    '''Parse command-line arguments.'''
+    parser = argparse.ArgumentParser(description='',
+                                     epilog='')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Enable verbose output (sets log level to DEBUG)')
 
     return parser.parse_args()
 
 def main():
     observability_finder()
     return
-
 
 if __name__ == '__main__':
     main()
